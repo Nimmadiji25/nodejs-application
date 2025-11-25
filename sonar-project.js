b765de394fe2686c86339eee27da17b24eb1ab3a
Nimmadiji25 <robertsoluwabunmi@gmail.com>
2025-11-24 19:11:53 -0600

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -1,13 +1,13 @@
 const sonarqubeScanner = require('sonarqube-scanner');
 
 sonarqubeScanner({
-  serverUrl: 'http://3.236.129.45:9000/',
+  serverUrl: 'http://172.31.24.252:9000/',
        options : {
 	    'sonar.projectDescription': 'This is a Node JS application',
 	    'sonar.projectName': 'Node JS Application - Sample',
 	    'sonar.projectKey':'NodeJsLandmarkTechnologies',
-	    'sonar.login': '3e9ee4f64a40359b3f9c904bc34392412e2c9d80',
-	    //'sonar.password': 'admin',
+	    'sonar.login': '0993a8d0d8cf90aae33ac7a42baf221a43749eab',
+	    //'sonar.password': 'sonaradmin',
             'sonar.projectVersion':'1.0',
 	    'sonar.language':'js',
             'sonar.sourceEncoding':'UTF-8',
